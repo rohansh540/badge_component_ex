9f837f8b90f73182fd6d652f66f01ddf36ca3c35
rohansh540 <91718209+rohansh540@users.noreply.github.com>
2021-11-03 11:14:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Badge from "./components/badge/index";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Badge />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/components/badge/index.js
@@ -0,0 +1,12 @@
+import "./badge.css";
+
+const badge = () => {
+  return (
+    <div className="badge">
+      <div>badge</div>
+      <span>2</span>
+    </div>
+  );
+};
+
+export default badge;
