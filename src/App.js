90f840143e3dc7d2615d9397529525a70157d43b
rohansh540 <91718209+rohansh540@users.noreply.github.com>
2021-11-17 11:27:45 +0530

    Value type handling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Badge label="Unread" value={4} variant="error" />
+      <Badge label="Unread" value="12" variant="error" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
--- src/components/badge/index.js
+++ src/components/badge/index.js
@@ -1,10 +1,17 @@
 import "./badge.css";
 
 const badge = ({ label, value, variant }) => {
+  const regex = /^\d+$/;
+  const newVal = Number.isInteger(value)
+    ? value
+    : regex.test(value)
+    ? value
+    : 0;
+
   return (
     <div className={`badge ` + variant}>
       <div>{label}</div>
-      <span>{value}</span>
+      <span>{newVal}</span>
     </div>
   );
 };
